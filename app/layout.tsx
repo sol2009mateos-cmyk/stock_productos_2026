882f7adf6f84927e30f227ac41e7f3e8a5aa23ff
sol2009mateos-cmyk <sol2009mateos@gmail.com>
2026-09-22 18:51:59 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css'
-import Sidebar from '@/components/Sidebar'
+import AppShell from '@/components/AppShell'
 
 export const metadata = {
   title: 'Stock Productos 2026',
@@ -14,10 +14,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body>
-        <div id="app-root" className="flex min-h-screen">
-          <Sidebar />
-          <main className="flex-1 p-8">{children}</main>
-        </div>
+        <AppShell>{children}</AppShell>
       </body>
     </html>
   )
