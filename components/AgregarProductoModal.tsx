ac0c65258965b9af6994fd93c25e021cc890bb33
sol2009mateos-cmyk <sol2009mateos@gmail.com>
2026-09-25 16:22:19 -0300

    Update AgregarProductoModal.tsx

--- components/AgregarProductoModal.tsx
+++ components/AgregarProductoModal.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
-import { supabase } from '@/lib/supabaseClient'
+import { createClient } from '@/lib/supabase/client'
 
 export default function AgregarProductoModal() {
   const router = useRouter()
